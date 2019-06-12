0293ab7f5885669676d8f9f1ce3d802557ab135c
amir-hemmati <ahemmati2018@gmail.com>
2019-06-12 11:31:15 -0700

    add plus plus width for scroll window

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,23 +5,32 @@ $(document).ready(function () {
         $("body").toggleClass("resizing");
     });
 
-    $(window).resize(function () { 
-        if($(window).width() > 680)
-        {
+    $(window).resize(function () {
+        if ($(window).width() > 680) {
             $(".nav-item").slideDown(0);
         }
     });
 
-    
 
-    $(window).scroll(function () { 
-        if($(this).scrollTop() > 200)
-        {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
             $("#nav").addClass("fixed");
         }
-        else{
+        else {
             $("#nav").removeClass("fixed");
         }
     });
     /* پایان کد اسلاید شدن منو و فیکس منو */
+
+    /* استارت اضافه شدن عرض با اسکرول  */
+    window.onscroll = function () { myFunction() };
+
+    function myFunction() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+    }
+    /* پایان اضافه شدن عرض با اسکرول  */
 });
